9fa2888948cd8c3eff5d7673ff3f64b7072396f8
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-08-01 10:48:44 -0400

    Time: 146 ms (93.57%), Space: 27.8 MB (82.49%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,38 +4,8 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         # Time Complexity: O(n)
-        # The below solution can potentially lead upto O(n^2) if the 
-        # the k vlaue is almost equal to n 
-        # Two Pointers
-        # i = 0 
-        # r = len(nums) - 1 
-        # while k!= 0:
-        #     nums.insert(i, nums[r])
-        #     nums.pop()
-        #     r = len(nums) - 1 
-        #     k -= 1 
-        # return nums 
-
-        # Time Compelxity: O(n)
-        # Space Complexity: O(1)
-        k = k % len(nums)
-        l , r = 0, len(nums) - 1 
-        while l < r:
-            # first we reverse the entire array
-            nums[l], nums[r] = nums[r], nums[l]
-            l , r = l+1, r-1 
-
-        l , r = 0, k - 1
-        # reverse the first k elements 
-        while l < r:
-            # first we reverse the entire array
-            nums[l], nums[r] = nums[r], nums[l]
-            l , r = l+1, r-1 
         
-        l , r = k, len(nums) - 1
-        # reverse the remaining portions of the elements 
-        while l < r:
-        # first we reverse the entire array
-            nums[l], nums[r] = nums[r], nums[l]
-            l , r = l+1, r-1 
-        
+        n = len(nums)
+        k = k % n  # Ensure k is within the bounds of the list length
+        nums[:] = nums[-k:] + nums[:-k]  # Rotate the li
+            
