a67f1b79c3d3655623bfb7e36ca4445f2f84886f
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-08-27 11:42:03 -0400

    Time: 68 ms (61.16%), Space: 17.3 MB (89.65%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
@@ -4,6 +4,7 @@ class Solution:
 
         while l < r and s[l] == s[r]:
             tmp = s[l]
+            # These loops "shrink" the string by removing consecutive matching characters from both ends until the characters at l and r differ or the pointers cross.
             while l <= r and s[l] == tmp:
                 l += 1
             while l <= r and s[r] == tmp:
