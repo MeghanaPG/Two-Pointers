fe7573d949ffe3706ac71c87435075f9e3ace0a6
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-08-28 12:35:48 -0400

    Time: 672 ms (70.65%), Space: 20.6 MB (71.08%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,27 +1,22 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-    # Sorting: O(nlogn) + Two sum O(n^2)
-    # Overall time complexity is O(n^2)
-        res = []
         nums.sort()
-
-        #To Handle the duplicate
+        res = []
         for i, a in enumerate(nums):
-            if i>0 and a == nums[i-1]:
-                continue 
-
-            # two sum
-            l,r = i+1, len(nums) - 1
+            if i > 0 and a == nums[i - 1]:
+                continue
+            l = i + 1
+            r = len(nums) - 1
             while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1 
-                elif threeSum < 0:
-                    l += 1 
-                else:
-                    res.append([a,nums[l],nums[r]])
+                curSum = a + nums[l] + nums[r]
+                if curSum > 0:
+                    r -= 1
+                elif curSum < 0:
                     l += 1
-                    while nums[l] == nums[l-1] and l < r: 
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    # However, if nums[l] == nums[l - 1], then using nums[l] in the next triplet would produce the same triplet as before.
+                    while l <= r and nums[l] == nums[l - 1]:
                         l += 1
-        return res 
-        
+        return res
